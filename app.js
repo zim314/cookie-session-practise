ea5bb02c5eb2cfecf0636dd74e5ec4131b4243fb
zim <qwe1472571@gmail.com>
2023-10-17 10:51:01 +0800

    新增 cookie練習

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
-app.use('/', (req, res) => {
-    res.send('起動成功')
+//設置簽名密碼
+app.use(cookieParser('orange'));
+
+
+app.get('/', (req, res) => {
+    res.send('歡迎來到首頁');
 })
 
-app.listen(3535, () => {
-    console.log('正在聆聽伺服器 port: 3535');
+app.get('/setCookie', (req, res) => {
+    res.cookie('yourCookie', 'strawberry', {signed: true});
+    res.send('已設置cookie');
 })
+
+app.get('/seeCookie', (req, res) => {
+    res.send(`目前設置的Cookie是: ${req.signedCookies.yourCookie}`);
+})
+
+app.listen(3535, () => console.log('正在聆聽伺服器 port: 3535'));
